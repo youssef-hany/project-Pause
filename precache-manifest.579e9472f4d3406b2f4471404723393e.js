4b02d6b85c2ce83da22fef44d73df377151c30f4
Youssef Hany <youssefhany30@yahoo.com>
2019-11-30 02:57:12 +0200

    Updates

---
+++ precache-manifest.579e9472f4d3406b2f4471404723393e.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12ba70948737fbd392afa646265f1045",
+    "url": "/project-Pause/index.html"
+  },
+  {
+    "revision": "cbc0175108efcdb5ebdd",
+    "url": "/project-Pause/static/css/main.06308b36.chunk.css"
+  },
+  {
+    "revision": "9d49ed15519be0d00bdd",
+    "url": "/project-Pause/static/js/2.04bfb457.chunk.js"
+  },
+  {
+    "revision": "cbc0175108efcdb5ebdd",
+    "url": "/project-Pause/static/js/main.da6efa39.chunk.js"
+  },
+  {
+    "revision": "bee0aba595880a0e426a",
+    "url": "/project-Pause/static/js/runtime-main.b4eda3cb.js"
+  },
+  {
+    "revision": "c230c5aefc582c44ea0400228a65f22c",
+    "url": "/project-Pause/static/media/Pause1.c230c5ae.jpg"
+  },
+  {
+    "revision": "96b888016fd1c31b6970d9977e495ab3",
+    "url": "/project-Pause/static/media/PauseLabel.96b88801.png"
+  },
+  {
+    "revision": "c43854a5c8f02fc1f1ddb148287b8d9e",
+    "url": "/project-Pause/static/media/PauseName.c43854a5.png"
+  },
+  {
+    "revision": "1a457dd2b06936503302f472ae4c6e0b",
+    "url": "/project-Pause/static/media/PauseSign.1a457dd2.png"
+  },
+  {
+    "revision": "7d074db59bfb3f3b16832542ae0f1c13",
+    "url": "/project-Pause/static/media/bookshelf349946_1280.7d074db5.jpg"
+  },
+  {
+    "revision": "2e654d8dc21607efc5a3ffaaf368e848",
+    "url": "/project-Pause/static/media/boxman2.2e654d8d.jpg"
+  },
+  {
+    "revision": "846e5b34cab10a11eb4ed2e1d2439b50",
+    "url": "/project-Pause/static/media/bueLogo.846e5b34.png"
+  },
+  {
+    "revision": "748a265467c0d6b8b19f5655523d445a",
+    "url": "/project-Pause/static/media/camera349948_1280.748a2654.jpg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/project-Pause/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "905dd89bf124ae0a4246ea4ff401e43d",
+    "url": "/project-Pause/static/media/purse407176_1280.905dd89b.jpg"
+  }
+]);
